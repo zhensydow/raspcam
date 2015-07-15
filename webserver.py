852ecd2af87daef9fabf677f285df85a1438d0de
Luis Cabellos <zhen.sydow@gmail.com>
2015-07-15 15:38:14 +0200

    update style

--- webserver.py
+++ webserver.py
@@ -5,6 +5,8 @@
 import web
 import config
 import json
+import multiprocessing
+import time
 
 #------------------------------------------------------------------------------
 URLS = (
@@ -22,30 +24,77 @@ web.config.debug = config.WEB_DEBUG
 #------------------------------------------------------------------------------
 class Main(object):
     """Class to Handle root urls."""
+
     def GET(self):
+        """http GET response method."""
+
         return TMPLS.main()
 
 
 #------------------------------------------------------------------------------
 class LastImage(object):
+    """Class to handle image queries."""
+
     def GET(self):
+        """http GET response method."""
+
         web.header("Content-Type", "images/jpeg")
-        return open("lastimage.jpg","rb").read()
+        return open("lastimage.jpg", "rb").read()
 
 
 #------------------------------------------------------------------------------
 class AjaxCamera(object):
+    """Class to handle camera ajax queries."""
+
     def PUT(self):
-        x = web.input()
-        print x
-        return json.dumps({'ok':True})
+        """http PUT response method."""
+
+        params = web.input()
+        cam_config = web.camera_config
+
+        if 'hflip' in params and params.hflip:
+            cam_config['hflip'] = not cam_config.get('hflip', False)
+
+        if 'vflip' in params and params.vflip:
+            cam_config['vflip'] = not cam_config.get('vflip', False)
+
+        return json.dumps({'ok': True})
+
+
+#------------------------------------------------------------------------------
+def camera_loop(camera_config):
+    """Camera update loop."""
+
+    print "start loop"
+    try:
+        while 1 == 1:
+            time.sleep(5)
+            print "camera ", camera_config
+
+    except KeyboardInterrupt:
+        print "Ending loop"
 
 
 #------------------------------------------------------------------------------
 def main():
-    """ Main function."""
+    """Main function."""
+
+    manager = multiprocessing.Manager()
+
+    camera_config = manager.dict()
+    camera_config['hflip'] = False
+    camera_config['vflip'] = False
+
     app = web.application(URLS, globals())
+
+    web.camera_config = camera_config
+
+    loop = multiprocessing.Process(target=camera_loop, args=(camera_config,))
+    loop.start()
+
     app.run()
+
+    loop.terminate()
 
 
 #------------------------------------------------------------------------------
